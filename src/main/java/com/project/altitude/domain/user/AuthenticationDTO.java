4a11314d1903aeb3ee00cf8ffb5efe53f3c2b1fd
la1ni <marilainarasilva@hotmail.com>
2024-08-05 13:29:21 -0300

    :sparkles: feat: User CRUD

--- src/main/java/com/project/altitude/domain/user/AuthenticationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.project.altitude.domain.user;
-
-public record AuthenticationDTO(String username, String password) {
-}
--- src/main/java/com/project/altitude/domain/user/RegisterDTO.java
+++
@@ -1,4 +0,0 @@
-package com.project.altitude.domain.user;
-
-public record RegisterDTO(String username, String password, String firstName, String lastName, String email, String phone, String cpf, UserRole userRole){
-}
